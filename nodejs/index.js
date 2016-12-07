023473b8da74ea2ecc7fe174c298ea03d514767b
Yuhua Chen <yuhuabill@gmail.com>
2016-12-06 23:35:14 -0800

    Add meta_map visualization, add trial list and detail pages

--- nodejs/index.js
+++ nodejs/index.js
@@ -4,6 +4,7 @@ var app = express();
 
 /** SQL Query **/
 var mysql = require("mysql");
+var pagination = require('pagination');
 
 /**
 localhost setting:
@@ -42,6 +43,52 @@ function db_connection(){
   	});	
 } 
 
+function google_chart(in_json ,number){
+	var count = 0;
+	var out_str = '';
+	out_str = '{';
+	out_str += 		'"cols":[{"id": "", "label": "Name", "pattern":"", "type": "string"},{"id": "", "label": "Percentage/Hits", "pattern":"", "type": "number"}],';
+	out_str += 		'"rows":['
+	var keys = Object.keys(in_json.terms);
+	keys.forEach(function(k){
+		if (count < number)
+			out_str +=  '{"c":[{"v":"'+k+'",},{"v":'+in_json.terms[k]+'}]},'	
+		count++;
+	})
+	out_str += '			]'
+	out_str += '}'
+	return out_str;
+}
+
+//Google Charts get file path by which_data
+function gc_get_file(which_data){
+	switch (which_data){
+		case "1": 
+			return "public/mm_type_precentage.json";
+		case "2":
+			return "public/mm_type_frequency.json";
+	}
+	return null;
+}
+
+function gc_get_title(which_data){
+	switch (which_data){
+		case "1":
+			return "Semantic Type Precentage";
+		case "2":
+			return "Term Hits";
+	}
+	return null;
+}
+
+function gc_res_render(which_data,number,response){
+	var title = gc_get_title(which_data);
+	response.render("pages/meta_map",{
+		'which_data':which_data,
+	  	'number':number,
+	  	'title': title});
+}
+
 db_connection();
 
 app.set('port', (process.env.PORT || 5000));
@@ -64,6 +111,99 @@ app.get("/demo", function(request,response){
 		});
 		data_loaded = true;
 	});		
+})
+
+var rowsPerPage = 20;
+var currentPage = 1;
+var totalResult = 6653;
+
+function createPaginator(currentPage, totalResult){
+	var boostrapPaginator = new pagination.TemplatePaginator({
+		prelink:'/trail', current: currentPage, rowsPerPage: rowsPerPage,
+		totalResult: totalResult, slashSeparator: true,
+		template: function(result) {
+			var i, len, prelink;
+			var html = '<div><ul class="pagination .pagination-lg">';
+			if(result.pageCount < 2) {
+				html += '</ul></div>';
+				return html;
+			}
+			prelink = this.preparePreLink(result.prelink);
+			if(result.previous) {
+				html += '<li><a href="' + prelink + result.previous + '">' + this.options.translator('PREVIOUS') + '</a></li>';
+			}
+			if(result.range.length) {
+				for( i = 0, len = result.range.length; i < len; i++) {
+					if(result.range[i] === result.current) {
+						html += '<li class="active"><a href="' + prelink + result.range[i] + '">' + result.range[i] + '</a></li>';
+					} else {
+						html += '<li><a href="' + prelink + result.range[i] + '">' + result.range[i] + '</a></li>';
+					}
+				}
+			}
+			if(result.next) {
+				html += '<li><a href="' + prelink + result.next + '" class="paginator-next">' + this.options.translator('NEXT') + '</a></li>';
+			}
+			html += '</ul></div>';
+			return html;
+		}
+	});
+	return boostrapPaginator;
+}
+
+app.get("/get_json/w/:which_data/n/:number", function(request,response){
+	var jsonfile = require('jsonfile')
+	var which_data = request.params.which_data;
+	var file = gc_get_file(which_data);
+	var number = request.params.number;
+	jsonfile.readFile(file, function(err, obj) {
+	  response.send(google_chart(obj,number));
+	})
+})
+
+// Metamap Visuals
+app.get("/meta_map/",function(request,response){
+	var which_data = 1;
+	var number = 20;
+	var title = gc_get_title(which_data);
+	gc_res_render(which_data,number,response);
+})
+
+app.get("/meta_map/w/:which_data/n/:number",function(request,response,next){
+	var which_data = request.params.which_data;
+	var number = request.params.number;
+	var title = gc_get_title(which_data);
+	gc_res_render(which_data,number,response);
+})
+
+app.get("/trail",function(request, response){
+	boostrapPaginator = createPaginator(1,totalResult)
+	con.query('SELECT * FROM med_info LIMIT 0,'+rowsPerPage+";" ,function(err,rows){
+		response.render('pages/trail',{
+				'results':rows,
+				'nav_render':boostrapPaginator.render()			
+		});
+	});
+})
+
+app.get("/trail/page/:pageNum", function(request, response, next){
+	pageNum = request.params.pageNum;
+	boostrapPaginator = createPaginator(pageNum,totalResult)
+	con.query('SELECT * FROM med_info LIMIT '+(rowsPerPage*(pageNum-1))+','+rowsPerPage+";" ,function(err,rows){
+		response.render('pages/trail',{
+				'results':rows,
+				'nav_render':boostrapPaginator.render()			
+		});
+	});
+})
+
+app.get("/trail/id/:id",function(request, response, next){
+	id = request.params.id;
+	con.query('SELECT * FROM med_info where id = '+id+";" ,function(err,rows){
+		response.render('pages/trail_single',{
+				'r':rows[0]		
+		});
+	});	
 })
 
 app.listen(app.get('port'), function() {
